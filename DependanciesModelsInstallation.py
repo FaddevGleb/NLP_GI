9869d6f74b498bfeb747f65254059236677b5918
FaddevGleb <pakaka191@gmail.com>
2025-02-06 14:51:22 +0300

    Add files via upload

---
+++ DependanciesModelsInstallation.py
@@ -0,0 +1,10 @@
+import os
+
+
+def DependenciesModelsInstall():
+    enCoreInstallCommand = "python -m spacy download en_core_web_sm"
+    ruCoreInstallCommand = "python -m spacy download ru_core_news_lg"
+    dependenciesInstall = "pip install -r dependencies.txt"
+    res1 = os.system(enCoreInstallCommand)
+    res2 = os.system(ruCoreInstallCommand)
+    res3 = os.system(dependenciesInstall)
