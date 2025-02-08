d5ed89f8847859d29dde51c96b312585a9e6eb6a
FaddevGleb <pakaka191@gmail.com>
2025-02-08 15:02:15 +0300

    Add files via upload
    UI with all wigets although no functionalyty

--- UI.py
+++ UI.py
@@ -6,6 +6,17 @@ class Window():
         self.window = 0
         self.selectedFile = ""
         self.selectedMode = ""
+        self.latinOnly = False
+        self.model = 0
+        self.resultType = 0
+    def FakePicture(self): #TEST ONLY! REMOVE AFTER!
+        texture_data = []
+        for i in range(0, 100 * 100):
+            texture_data.append(255 / 255)
+            texture_data.append(0)
+            texture_data.append(255 / 255)
+            texture_data.append(255 / 255)
+        return texture_data
 
     def ToggleWindow(self, page=0):
         match page:
@@ -17,12 +28,153 @@ class Window():
                 dpg.set_primary_window("ModeChoice", True)
             case 3:
                 dpg.set_primary_window("PreparationChoice", True)
+            case 4:
+                dpg.set_primary_window("FiltModResChoice", True)
+            case 5:
+                dpg.set_primary_window("TopResults", True)
+            case 6:
+                dpg.set_primary_window("FileResults", True)
+            case 9:
+                dpg.set_primary_window("GraphResults", True)
+            case 8:
+                dpg.set_primary_window("VectorDialogue", True)
+            case 7:
+                 dpg.set_primary_window("AllResults", True)
         dpg.configure_item("Greeting", show=(page == 0))
         dpg.configure_item("FileChoice", show=(page == 1))
         dpg.configure_item("ModeChoice", show=(page == 2))
         dpg.configure_item("PreparationChoice", show=(page == 3))
+        dpg.configure_item("FiltModResChoice", show=(page == 4))
+        dpg.configure_item("TopResults", show=(page == 5))
+        dpg.configure_item("FileResults", show=(page == 6))
+        dpg.configure_item("GraphResults", show=(page == 9))
+        dpg.configure_item("VectorDialogue", show=(page == 8))
+        dpg.configure_item("AllResults", show=(page == 7))
+
+
 
     def CreateWindows(self):
+        def CreateTopResultWindow():
+
+            def GraphTopResult(sender, appData):
+                self.ToggleWindow(9)
+
+            def ToFiltModResChoice():
+                self.ToggleWindow(4)
+
+            with dpg.window(label="Top 25 results", no_collapse=True, width=800, height=600, no_move=True, tag="TopResults"):
+                with dpg.group(horizontal=False):
+                    dpg.add_text("HERE SHOULD BE RESULTS")
+                    dpg.add_button(label="See the answer as a graph.", callback=GraphTopResult)
+
+                dpg.add_button(label="Back", width=100, callback=ToFiltModResChoice, tag="BFiltModResChoiceTopResultsWindow")
+
+        def CreateFileResultWindow():
+            def GraphTopResult(sender, appData):
+                self.ToggleWindow(9)
+
+            def ToFiltModResChoice():
+                self.ToggleWindow(4)
+
+            with dpg.window(label="Top 25 results", no_collapse=True, width=800, height=600, no_move=True,
+                            tag="FileResults"):
+                with dpg.group(horizontal=False):
+                    dpg.add_text("Enter the filename for a result file")
+                    dpg.add_input_text(label="filename")
+                    dpg.add_button(label="See the answer as a graph.", callback=GraphTopResult)
+
+                dpg.add_button(label="Back", width=100, callback=ToFiltModResChoice, tag="BFiltModResChoiceFileResultsWindow")
+
+        def CreateGraphResultWindow():
+            with dpg.window(label="Graph results", no_collapse=True, width=800, height=600, no_move=True,
+                            tag="GraphResults"):
+
+                with dpg.texture_registry(show=False):
+                    dpg.add_static_texture(width=100, height=100, default_value=self.FakePicture(), tag="texture_tag")
+
+                dpg.add_image("texture_tag")
+
+        def CreateVectorResultDialogueWindow():
+
+            def ToFiltModResChoice():
+                self.ToggleWindow(4)
+
+            def GraphTopResult(sender, appData):
+                self.ToggleWindow(9)
+
+            with dpg.window(label="Vector dialogue", no_collapse=True, width=800, height=600, no_move=True,
+                            tag="VectorDialogue"):
+                with dpg.group(horizontal=False):
+                    dpg.add_text("Enter the word in raw text format")
+                    dpg.add_input_text(label="word")
+                    dpg.add_button(label="See the answer as a graph.", callback=GraphTopResult)
+
+                dpg.add_button(label="Back", width=100, callback=ToFiltModResChoice, tag="BFiltModResChoiceVectorResultWindow")
+
+
+        def CreatePrintAllResultWindow():
+
+            def ToFiltModResChoice():
+                self.ToggleWindow(4)
+
+            def GraphTopResult(sender, appData):
+                self.ToggleWindow(9)
+
+            with dpg.window(label="Top 25 results", no_collapse=True, width=800, height=600, no_move=True,
+                            tag="AllResults"):
+                with dpg.group(horizontal=False):
+                    dpg.add_text("HERE SHOULD BE RESULTS")
+                    dpg.add_button(label="See the answer as a graph.", callback=GraphTopResult)
+
+                dpg.add_button(label="Back", width=100, callback=ToFiltModResChoice, tag="BFiltModResChoiceARW")
+
+
+        def CreateModelFilteringResultChoiceWindow():
+            with dpg.window(label="Filtering, Model & Result type Choice", no_collapse=True, width=800, height=600, no_move=True,
+                            tag="FiltModResChoice"):
+
+                def ToPreparationChoice():
+                    self.ToggleWindow(3)
+
+                def ToResultChoice():
+                    self.ToggleWindow(self.resultType + 5)
+
+                def CallbackModelChoiceEn(sender, appData):
+                    print(sender, appData)
+                    self.model = 0
+
+                def CallbackModelChoiceRu(sender, appData):
+                    self.model = 1
+
+                def CallbackResultChoiceTop25(sender, appData):
+                    self.resultType = 0
+
+                def CallbackResultChoiceFile(sender, appData):
+                    self.resultType = 1
+
+                def CallbackResultChoiceAll(sender, appData):
+                    self.resultType = 2
+
+                def CallbackResultChoiceVector(sender, appData):
+                    self.resultType = 3
+
+                with dpg.group(horizontal=True):
+                    with dpg.group(horizontal=False):
+                        dpg.add_text("Select model")
+                        dpg.add_button(label="en_core_web_lg", callback=CallbackModelChoiceEn)
+                        dpg.add_button(label="ru_core_news_lg", callback=CallbackModelChoiceRu)
+
+                    with dpg.group(horizontal=False):
+                        dpg.add_text("Select result format")
+                        dpg.add_button(label="top 25", callback=CallbackResultChoiceTop25)
+                        dpg.add_button(label="store in a file", callback=CallbackResultChoiceFile)
+                        dpg.add_button(label="print all", callback=CallbackResultChoiceAll)
+                        dpg.add_button(label="vector", callback=CallbackResultChoiceVector)
+
+                with dpg.group(horizontal=True):
+                    dpg.add_button(label="Back", width=100, callback=ToPreparationChoice, tag="BtoPreparationChoice")
+                    dpg.add_button(label="Result", width=100, callback=ToResultChoice, tag="FtoResult")
+
         def CreatePreparationChoiceWindow():
             with dpg.window(label="Text Preparation Choice", no_collapse=True, width=800, height=600, no_move=True, tag="PreparationChoice"):
                 dpg.add_text(
@@ -34,18 +186,27 @@ skip: Do nothing and skip processing the text")
                 def ToModeChoice():
                     self.ToggleWindow(2)
 
+                def ToFiltModResChoice():
+                    self.ToggleWindow(4)
+
                 def CallbackModeChoice(sender, appData):
                     print(appData, sender)
                     self.selectedMode = sender - 296
 
-                with dpg.group(horizontal=False):
-                    dpg.add_button(label="ru", width=150, callback=CallbackModeChoice)
-                    dpg.add_button(label="en", width=150, callback=CallbackModeChoice)
-                    dpg.add_button(label="skip", width=150, callback=CallbackModeChoice)
+                def CallbackLatinOnly(sender, appData):
+                    print(sender, appData)
+                    self.latinOnly = appData
+
+                with dpg.group(horizontal=True):
+                    with dpg.group(horizontal=False):
+                        dpg.add_button(label="ru", width=150, callback=CallbackModeChoice)
+                        dpg.add_button(label="en", width=150, callback=CallbackModeChoice)
+                        dpg.add_button(label="skip", width=150, callback=CallbackModeChoice)
+                    dpg.add_checkbox(label="Leave only basic latin?(en only)", default_value=False, callback=CallbackLatinOnly)
 
                 with dpg.group(horizontal=True):
                     dpg.add_button(label="Back", width=100, callback=ToModeChoice, tag="BtoModeChoice")
-                    dpg.add_button(label="Next", width=100, tag="Fto")
+                    dpg.add_button(label="Next", width=100, callback=ToFiltModResChoice, tag="FtoFiltModResChoice")
 
         def CreateModeChoiceWindow():
             with dpg.window(label="Mode Choice", no_collapse=True, width=800, height=600, no_move=True, tag="ModeChoice"):
@@ -78,7 +239,6 @@ skip: Do nothing and skip processing the text")
                 dpg.add_text(
                     "Select a text file encoded with UTF-8. If later on you encounter an error specific to\nyour input file try to edit the file first.", parent="FileChoice", tag="fileChoiceText")
 
-                # Создаем файловый браузер с фильтром .txt
                 def _callback_file_selected(sender, app_data):
                     print("Selected file:", app_data[0])
                     selectedFile = app_data[0]
@@ -124,7 +284,12 @@ skip: Do nothing and skip processing the text")
         CreateFileChoiceWindow()
         CreateModeChoiceWindow()
         CreatePreparationChoiceWindow()
-
+        CreateModelFilteringResultChoiceWindow()
+        CreateFileResultWindow()
+        CreateVectorResultDialogueWindow()
+        CreateGraphResultWindow()
+        CreateTopResultWindow()
+        CreatePrintAllResultWindow()
 def LoadFonts():
     with dpg.font_registry():
         # first argument ids the path to the .ttf or .otf file
