3cc4b89f98b037cd7ebf0cc39c8bdf714facd735
FaddevGleb <pakaka191@gmail.com>
2025-03-02 14:28:42 +0300

    DependanciesModelsInstallation.py

--- DependanciesModelsInstallation.py
+++ DependanciesModelsInstallation.py
@@ -2,7 +2,7 @@ import os
 
 
 def DependenciesModelsInstall():
-    enCoreInstallCommand = "python -m spacy download en_core_web_sm"
+    enCoreInstallCommand = "python -m spacy download en_core_web_lg"
     ruCoreInstallCommand = "python -m spacy download ru_core_news_lg"
     dependenciesInstall = "pip install -r dependencies.txt"
     res1 = os.system(enCoreInstallCommand)
